e287ece324272785507e164edecf803661304939
its_michvyt <147197452+itsmichvyt@users.noreply.github.com>
2025-01-18 12:15:53 +0000

    Create KeySystem.lua

---
+++ KeySystem.lua
@@ -0,0 +1,22 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Key System",
+   Icon = 0,
+   LoadingTitle = "",
+   LoadingSubtitle = "by Mich",
+   Theme = "Default",
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false,
+
+   KeySystem = true,
+   KeySettings = {
+      Title = "Key System",
+      Subtitle = "Loader GUI",
+      Note = "Get the key from the developer or Mich",
+      FileName = "",
+      SaveKey = false,
+      GrabKeyFromSite = false,
+      Key = {"LvN14r9CkLvE8f0UGkWo"}
+   }
+})
